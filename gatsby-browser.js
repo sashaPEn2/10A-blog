09fb64fd4039f92f351d00411c6c660b0694a96b
sashaPEn2 <113623246+sashaPEn2@users.noreply.github.com>
2023-10-11 21:16:27 +0300

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
+    `Этот сайт был обновлен. ` +
+      `Перезагрузить, чтобы отобразить последнюю версию?`
   )
 
   if (answer === true) {
