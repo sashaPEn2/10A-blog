e14005993a642fc50bf60f5f07d2c81f8a9c49e6
sashaPEn2 <113623246+sashaPEn2@users.noreply.github.com>
2024-01-02 19:25:17 +0300

    2522

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -63,6 +63,35 @@ class Navigation extends React.Component {
             <RiCloseLine />
           </div>
         </button>
+        <script>
+   window.onload = function() {
+      window.YaAuthSuggest.init({
+                  client_id: 'c46f0c53093440c39f12eff95a9f2f93',
+                  response_type: 'token',
+                  redirect_uri: 'https://examplesite.com/suggest/token'
+               },
+               'https://examplesite.com', {
+                  view: 'button',
+                  parentId: 'container',
+                  buttonView: 'main',
+                  buttonTheme: 'light',
+                  buttonSize: 'm',
+                  buttonBorderRadius: 12
+               }
+            )
+            .then(function(result) {
+               return result.handler()
+            })
+            .then(function(data) {
+               console.log('Сообщение с токеном: ', data);
+               document.body.innerHTML += `Сообщение с токеном: ${JSON.stringify(data)}`;
+            })
+            .catch(function(error) {
+               console.log('Что-то пошло не так: ', error);
+               document.body.innerHTML += `Что-то пошло не так: ${JSON.stringify(error)}`;
+            })};
+    
+   </script>
         <ul>
           {listMenuItems}
           <div sx={navStyle.border}></div>
