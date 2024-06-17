13bdb4fd0094318e5c572e4bc401039cc77e44f9
sashaPEn2 <113623246+sashaPEn2@users.noreply.github.com>
2024-06-17 17:36:32 +0000

    54

--- src/pages/events.js
+++ src/pages/events.js
@@ -17,24 +17,18 @@ const NotFound = () => (
       }}
     >
       <header>
-        <div class="event-2"><MdEvent
-          style={{
-            fontSize: "64px",
-            color: "var(--primary-color)",
-          }}
-        />
-        <h1>Мероприятия</h1>
-        </div>
-  <table class="events">
-  <thead>
+        
+        <center></center>
+        <table class="events">
+<thead>
   <tr>
     <th class="tg-0pky">Дата</th>
     <th class="tg-0pky">Название мероприятия</th>
     <th class="tg-0pky">Время</th>
     <th class="tg-0pky">Место</th>
   </tr>
-  </thead>
-  <tbody>
+</thead>
+<tbody>
   <tr>
     <td class="tg-0pky">01.11.2023</td>
     <td class="tg-0pky"><a href="https://blog-10a.netlify.app/museum-david" target="_blank" rel="noopener noreferrer">Экскурсия в музей истории Давид-Городка</a></td>
@@ -47,13 +41,13 @@ const NotFound = () => (
     <td class="tg-0pky">18:00</td>
     <td class="tg-0pky">Кинотеатр «Экран»</td>
   </tr>
-  <tr>
+<tr>
     <td class="tg-0lax">18.11.2023</td>
     <td class="tg-0lax"><a href="https://blog-10a.netlify.app/quiz" target="_blank" rel="noopener noreferrer">Интеллектуальная викторина</a></td>
     <td class="tg-0lax">11:20</td>
     <td class="tg-0lax">Кабинет №32</td>
   </tr>
-  <tr>
+<tr>
     <td class="tg-0lax">01.12.2023</td>
     <td class="tg-0lax"><a href="https://blog-10a.netlify.app/quiz-2023" target="_blank" rel="noopener noreferrer">Молодежный квиз 2023</a></td>
     <td class="tg-0lax">13:20</td>
@@ -71,13 +65,14 @@ const NotFound = () => (
     <td class="tg-0lax">12:00</td>
     <td class="tg-0lax">Кабинет №32</td>
   </tr>
-  <tr>
-    <td class="tg-0lax">01.02.2024</td>
-    <td class="tg-0lax"><a href="https://blog-10a.netlify.app/bibliotheca/1-march" target="_blank" rel="noopener noreferrer">1 марта — Международный день борьбы с наркоманией и наркобизнесом</a></td>
-    <td class="tg-0lax">13:20</td>
+ <tr>
+    <td class="tg-0lax">01.03.2024</td>
+    <td class="tg-0lax"><a href="https://blog-10a.netlify.app/bibliotheca/1-march" target="_blank" rel="noopener noreferrer">1 марта — Международный день борьбы с наркоманией и наркобизнесом</a>
+</td>
+<td class="tg-0lax">13:20</td>
     <td class="tg-0lax">Центральная библиотека</td>
   </tr>
-  <tr>
+<tr>
     <td class="tg-0lax">02.03.2024</td>
     <td class="tg-0lax"><a href="https://blog-10a.netlify.app/pages-about-history" target="_blank" rel="noopener noreferrer">Квест «По страницам истории»</a></td>
     <td class="tg-0lax">11:30</td>
@@ -85,21 +80,21 @@ const NotFound = () => (
   </tr>
     <tr>
     <td class="tg-0lax">16.03.2024</td>
-    <td class="tg-0lax">15 марта — День Конституции Республики Беларусь</td>
+    <td class="tg-0lax"><a href="https://blog-10a.netlify.app/Constitution-Day">15 марта — День Конституции Республики Беларусь</a></td>
     <td class="tg-0lax">13:20</td>
     <td class="tg-0lax">Центральная библиотека</td>
   </tr>
   <tr>
     <td class="tg-0lax">20.04.2024</td>
-    <td class="tg-0lax">Психологический тест «Дороги, которые мы выбираем»</td>
+    <td class="tg-0lax"><a href="https://blog-10a.netlify.app/career-guidance-test">Психологический тест «Дороги, которые мы выбираем»</a></td>
     <td class="tg-0lax">12:00</td>
     <td class="tg-0lax">Кабинет №32</td>
   </tr>
 </tbody>
 </table>
 
-<h2>Дни здоровья</h2>
-<center><table class="events-health">
+<h2>Дни здоровья<a href="#cite-1">¹</a></h2>
+<table class="events-health">
 <thead>
   <tr>
     <th class="tg-0pky">Дата</th>
@@ -141,8 +136,8 @@ const NotFound = () => (
   </tr>
 </tbody>
 </table>
-</center>
-<h2>Общегимназические мероприятия</h2>
+
+<h2>Общегимназические мероприятия<a href="#cite-1">¹</a></h2>
 
 <center><table class="global-events">
 <thead>
@@ -157,15 +152,16 @@ const NotFound = () => (
     <td class="tg-0lax"><a href="https://blog-10a.netlify.app/yarmarka-2023" target="_blank" rel="noopener noreferrer">Ярмарка талантов 2023</a></td>
   </tr>
   <tr>
-    <td class="tg-0lax">16.03</td>
+    <td class="tg-0lax">21.03</td>
     <td class="tg-0lax">Мисс гимназии 2024</td>
   </tr>
   <tr>
     <td class="tg-0lax">18.05</td>
-    <td class="tg-0lax">День гимназии</td>
+    <td class="tg-0lax"><a href="https://blog-10a.netlify.app/den-gimnazii" target="_blank" rel="noopener noreferrer">День гимназии</a></td>
   </tr>
 </tbody>
 </table>
+<cite><a id="cite-1">¹</a> — по данным сайта <a href="https://gim.roo-stolin.gov.by/%D1%83%D1%87%D0%B0%D1%89%D0%B8%D0%BC%D1%81%D1%8F/%D1%88%D0%B5%D1%81%D1%82%D0%BE%D0%B9-%D1%88%D0%BA%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%B4%D0%B5%D0%BD%D1%8C">Столинской государственной гимназии.</a></cite>
 </center>
       </header>
       <Link to="/" className="button">
