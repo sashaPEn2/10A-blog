961c0b421a3f6ab93b1044d82741311675bb2881
sashaPEn2 <113623246+sashaPEn2@users.noreply.github.com>
2025-01-25 18:58:42 +0000

    `Update theme variables, styles, and component styles`

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,15 +6,16 @@ const Banner = () => {
       position: 'fixed',
       bottom: '20px',
       right: '20px',
-      background: 'linear-gradient(to right,rgb(201, 0, 0),rgb(247, 7, 7),rgb(30, 182, 10))',
+      background: 'linear-gradient(900deg,rgb(201, 0, 0),rgb(247, 7, 7),rgb(30, 182, 10))',
       color: 'white',
       padding: '10px 20px',
       borderRadius: '10px',
       zIndex: 1000,
       fontSize: '16px',
       textAlign: 'center',
+      cursor: 'pointers'
     }}>
-      Читаете блог? Не забудьте про выборы! Ваш голос важнее!
+Читаете блог? Не забудьте про выборы! Ваш голос важнее!
     </div>
   );
 };
--- src/components/footer.js
+++ src/components/footer.js
@@ -12,9 +12,7 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        2025 © 10А Столинской государственной гимназии. <a href="https://blog-10a.netlify.app/admin" style={{ 
-        color: 'white',
-        }}>Вход для администраторов</a></p>
+        2025 © 10А Столинской государственной гимназии. <a href="https://blog-10a.netlify.app/admin">Вход для администраторов</a></p>
     </div>
   </footer>
 )
